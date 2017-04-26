1db549348508fc8c7c84df2053c231563da1b21b
¨Goodness <gtkbrain@gmail.com>
2017-04-26 20:56:09 +0100

    Initial commit with Splash Screen handled

---
+++ app/src/main/java/me/goodnesskayode/flippad/SplashScreen.java
@@ -0,0 +1,26 @@
+package me.goodnesskayode.flippad;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent main = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(main);
+                finish();
+            }
+        },SPLASH_SCREEN_TIME_OUT);
+    }
+}
